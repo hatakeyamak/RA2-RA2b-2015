62288177653ab405d80e258b97662ecb1a84c4ca
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2015-10-16 18:07:26 -0500

    macro for correction - update. Some histogram ranges extended to 19 as they should be.

--- New_RA2b_2015/macros/plot_IsoTrkEff_LowDphi_allSamples.C
+++ New_RA2b_2015/macros/plot_IsoTrkEff_LowDphi_allSamples.C
@@ -2,21 +2,17 @@
 void plot_IsoTrkEff_LowDphi_allSamples(){
 
   //
-  // icomp=0: only show own results
-  //       1: show also Koushik's results
-  //
-  
-  //
   ///////////////////////////////////////////////////////////////////////////////////////////
   ////Some cosmetic work for official documents. 
   gStyle->SetOptStat(0);  ///to avoid the stat. on the plots 
   gROOT->LoadMacro("tdrstyle.C");
-//  setTDRStyle();
-//  gROOT->LoadMacro("CMS_lumi_v2.C");
+  gStyle->SetLineWidth(2); // Default:1 --- Line width of axis
+  //setTDRStyle();
+  //gROOT->LoadMacro("CMS_lumi_v2.C");
 
   char tempname[200];
   char tempname2[200];
-  int W = 600;
+  int W = 800;
   int H = 600;
   int H_ref = 600;
   int W_ref = 800;
@@ -34,12 +30,12 @@ void plot_IsoTrkEff_LowDphi_allSamples(){
   c1->SetRightMargin( R/W );
   c1->SetTopMargin( T/H );
   c1->SetBottomMargin( B/H );
-  c1->SetTickx(0);
-  c1->SetTicky(0);
+  c1->SetTickx(1);
+  c1->SetTicky(1);
   
   Float_t legendX1 = .65; //.50;
   Float_t legendX2 = .90; //.70;
-  Float_t legendY1 = .75; //.65;
+  Float_t legendY1 = .70; //.65;
   Float_t legendY2 = .85;
   TLegend* catLeg1 = new TLegend(legendX1,legendY1,legendX2,legendY2);
   catLeg1->SetTextSize(0.038);
@@ -58,62 +54,104 @@ void plot_IsoTrkEff_LowDphi_allSamples(){
 
   TH1D * thist, * thist2, * thist3;
   THStack * tempstack;
-//  catLeg1->SetHeader("Prob. of #mu from #tau ");
+  //  catLeg1->SetHeader("Prob. of #mu from #tau ");
 
 
-//...........................................................................//
-// TTbar ....................................................................//
-//...........................................................................//
-    double XUp = 18. , maxVal=3.;
+  //...........................................................................//
+  // TTbar ....................................................................//
+  //...........................................................................//
+  double XUp = 19., maxVal=1.4, minVal=0.2;
 
-    sprintf(tempname,"IsoEff_lowDphi");
+  sprintf(tempname,"IsoEff_lowDphi");
 	
-	thist3 = (TH1D*)file_Total->Get(tempname)->Clone();
-	thist3->GetXaxis()->SetRangeUser(1.,XUp);
-    thist3->SetMaximum(maxVal);
-    thist3->SetTitle("");
-    thist3->GetXaxis()->SetLabelFont(42);
-    thist3->GetXaxis()->SetLabelOffset(0.007);
-    thist3->GetXaxis()->SetLabelSize(0.04);
-    thist3->GetXaxis()->SetTitleSize(0.04);
-    thist3->GetXaxis()->SetTitleOffset(0.9);
-    thist3->GetXaxis()->SetTitleFont(42);
-    thist3->GetYaxis()->SetLabelFont(42);
-    thist3->GetYaxis()->SetLabelOffset(0.007);
-    thist3->GetYaxis()->SetLabelSize(0.04);
-    thist3->GetYaxis()->SetTitleSize(0.04);
-    thist3->GetYaxis()->SetTitleOffset(1.25);
-    thist3->GetYaxis()->SetTitleFont(42);
-    thist3->GetXaxis()->SetTitle("search bin");
-    thist3->GetYaxis()->SetTitle("Low #Delta#Phi IsoTrk efficiency");
-    thist3->SetMaximum(maxVal);
-    thist3->SetLineColor(3);
-	thist3->SetLineStyle(2);
-	thist3->SetLineWidth(2);
-//    thist3->SetFillColor(0);
-//    thist3->SetLineWidth(3);
-    thist3->Draw("same");
-
+  thist3 = (TH1D*)file_Total->Get(tempname)->Clone();
+  thist3->GetXaxis()->SetRangeUser(1.,XUp);
+  thist3->SetMaximum(maxVal);
+  thist3->SetMinimum(minVal);
+  thist3->SetTitle("");
+  thist3->GetXaxis()->SetLabelFont(42);
+  thist3->GetXaxis()->SetLabelOffset(0.007);
+  thist3->GetXaxis()->SetLabelSize(0.04);
+  thist3->GetXaxis()->SetTitleSize(0.05);
+  thist3->GetXaxis()->SetTitleOffset(0.9);
+  thist3->GetXaxis()->SetTitleFont(42);
+  thist3->GetYaxis()->SetLabelFont(42);
+  thist3->GetYaxis()->SetLabelOffset(0.007);
+  thist3->GetYaxis()->SetLabelSize(0.04);
+  thist3->GetYaxis()->SetTitleSize(0.05);
+  thist3->GetYaxis()->SetTitleOffset(1.0);
+  thist3->GetYaxis()->SetTitleFont(42);
+  thist3->GetXaxis()->SetTitle("Search bin");
+  thist3->GetYaxis()->SetTitle("Isotrack veto efficiency (low #Delta#phi)");
+  thist3->SetMaximum(maxVal);
+  thist3->SetLineColor(3);
+  thist3->SetLineStyle(2);
+  thist3->SetLineWidth(2);
+  //    thist3->SetFillColor(0);
+  thist3->SetLineWidth(3);
+  thist3->Draw("same");
 	
-    thist2 = (TH1D*)file_WJet->Get(tempname)->Clone();
-    thist2->SetLineColor(2);
-    thist2->Draw("same");
-
-    thist = (TH1D*)file_TTbar->Get(tempname)->Clone();
-    thist->SetLineColor(1);
-//    thist->SetFillColor(0);
-//    thist->SetLineWidth(3);
-    thist->Draw("same");
-	
-	
-    sprintf(tempname,"T#bar{T}");
-    catLeg1->AddEntry(thist,tempname,"l");
-    sprintf(tempname,"WJet");
-    catLeg1->AddEntry(thist2,tempname,"l"); 
-	catLeg1->AddEntry(thist3,"TTbar + WJet","l");
-    catLeg1->Draw();
+  thist2 = (TH1D*)file_WJet->Get(tempname)->Clone();
+  thist2->SetLineColor(2);
+  thist2->SetLineWidth(3);
+  thist2->Draw("same");
   
-    sprintf(tempname,"IsoEfficiencies_LowDphi_allSamples.png");
-    c1->Print(tempname);
+  thist = (TH1D*)file_TTbar->Get(tempname)->Clone();
+  thist->SetLineColor(1);
+  //    thist->SetFillColor(0);
+  thist->SetLineWidth(3);
+  thist->Draw("same");
+	
+  thist3->Draw("same");
+
+  sprintf(tempname,"t#bar{t}");
+  catLeg1->AddEntry(thist,tempname,"l");
+  sprintf(tempname,"W+jets");
+  catLeg1->AddEntry(thist2,tempname,"l"); 
+  catLeg1->AddEntry(thist3,"t#bar{t} & W+jets","l");
+  catLeg1->Draw();
+  
+  //
+  // Labels for binnings
+  //
+
+  double ymax=1.0;
+  double ymin=0.2;
+  double ytext=0.95;
+
+  TLatex * ttext1 = new TLatex(4. , ytext , "4 #leq N_{jets} #leq 6");
+  ttext1->SetTextFont(42);
+  ttext1->SetTextSize(0.04);
+  ttext1->SetTextAlign(22);
+  ttext1->Draw();
+
+  TLatex * ttext2 = new TLatex(10. , ytext , "7 #leq N_{jets} #leq 8");
+  ttext2->SetTextFont(42);
+  ttext2->SetTextSize(0.04);
+  ttext2->SetTextAlign(22);
+  ttext2->Draw();
+
+  TLatex * ttext3 = new TLatex(16. , ytext , "N_{jets} #geq 9");
+  ttext3->SetTextFont(42);
+  ttext3->SetTextSize(0.04);
+  ttext3->SetTextAlign(22);
+  ttext3->Draw();
+
+  TLine *tline_1 = new TLine(7.,ymin,7.,ymax);
+  tline_1->SetLineStyle(2);
+  tline_1->Draw();
+
+  TLine *tline_2 = new TLine(13.,ymin,13.,ymax);
+  tline_2->SetLineStyle(2);
+  tline_2->Draw();
+
+  //
+  // Save output
+  //
+
+  sprintf(tempname,"IsoEfficiencies_LowDphi_allSamples.png");
+  c1->Print(tempname);
+  sprintf(tempname,"IsoEfficiencies_LowDphi_allSamples.pdf");
+  c1->Print(tempname);
 
 }
