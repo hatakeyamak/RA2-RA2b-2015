0d92e92ad40b6c62fc52e34dcd3495cab582358d
ahmad <ahmad_borzou@baylor.edu>
2016-06-17 11:08:41 -0500

    a lost file recovered

---
+++ New_RA2b_2015/runmanyData.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+sample=$1
+outputStr=$2
+submitscript=submitScriptData.sh
+
+njobs=`ls InputFiles_Data/filelist_data_SingleMuon_${sample}_* | wc -l`
+
+#njobs=$[$njobs+1]
+echo number of jobs: $njobs
+mkdir -p qsub
+#for i in `seq 1 $njobs`; do
+for i in `seq 0 $njobs`; do
+
+export filenum=$i
+export sample=$sample
+export outputStr=$outputStr
+echo $filenum
+echo $code 
+
+qsub -N Data -o qsub/ -e qsub/ -V $submitscript 
+
+sleep 1
+
+done
+
