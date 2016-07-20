dcf1e0efbef199eb72523edfa54ce0ace071e166
Aditee Prabhakar Rane - Dr. Hatakeyama collaborator <arane@kodiak.baylor.edu>
2016-07-20 01:45:44 -0500

    tauhad_template2.cpp and adjustUncertainties.C added

---
+++ New_RA2b_2015/adjustUncertainties.C
@@ -0,0 +1,286 @@
+/*
+  Plots the various systematics and adjusts the bins with large fluctuations.
+  > root -l -b 
+  > .L adjustUncertainties()
+  > adjustUncertainties(true) // to adjust fluctuating bins, will create ARElog49_HadTauEstimation_data_formatted_New.root and data_formatted_systematics_new.pdf 
+  > adjustUncertainties(false) // for original hists, will create ARElog49_HadTauEstimation_data_formatted_NoChange.root and data_formatted_systematics_NoChange.pdf
+*/
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include "TH1.h"
+#include "TH1D.h"
+
+enum icol {ic_up=kRed, ic_dn=kBlue};
+
+bool firstPrint=true;
+bool lastPrint =true;
+
+void adjustUncertainties(bool adjust=true) {
+
+  TFile *file = TFile::Open("ARElog49_HadTauEstimation_data_formatted.root");
+
+  TFile *fout = TFile::Open("ARElog49_HadTauEstimation_data_formatted_New.root", "RECREATE");
+  if(adjust==false)
+    fout = TFile::Open("ARElog49_HadTauEstimation_data_formatted_NoChange.root", "RECREATE");
+  
+  std::vector<TCanvas *> cc;
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "searchBin_BMistag") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "searchBin_MuRecoSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "searchBin_MuIsoSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "searchBin_MuRecoIso") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "searchBin_JECSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "searchBin_MTSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "searchBin_AccSysPDF") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "searchBin_AccSysScale") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_MtEffStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_IsoTrkVetoEffUncertaintyStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_AccStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_MuFromTauStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_DileptonUncertainty") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_TrigEffUncertainty") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_closureUncertainty") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_closureUncertainty_adhoc") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_StatUncertainties") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "searchBin_nominal_fullstatuncertainty") );
+
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_HiDphi_BMistag") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_HiDphi_MuRecoSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_HiDphi_MuIsoSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_HiDphi_MuRecoIso") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_HiDphi_JECSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_HiDphi_MTSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_HiDphi_AccSysPDF") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_HiDphi_AccSysScale") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_MtEffStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_IsoTrkVetoEffUncertaintyStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_AccStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_MuFromTauStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_DileptonUncertainty") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_TrigEffUncertainty") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_closureUncertainty") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_closureUncertainty_adhoc") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_StatUncertainties") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_HiDphi_nominal_fullstatuncertainty") );
+
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_LowDphi_BMistag") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_LowDphi_MuRecoSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_LowDphi_MuIsoSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_LowDphi_MuRecoIso") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_LowDphi_JECSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_LowDphi_MTSys") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_LowDphi_AccSysPDF") );
+  cc.push_back( adjustUncertaintiesUpDn(file, fout,  adjust, "QCDBin_LowDphi_AccSysScale") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_MtEffStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_IsoTrkVetoEffUncertaintyStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_AccStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_MuFromTauStat") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_DileptonUncertainty") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_TrigEffUncertainty") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_closureUncertainty") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_closureUncertainty_adhoc") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_StatUncertainties") );
+  cc.push_back( adjustUncertainties    (file, fout,  adjust, "QCDBin_LowDphi_nominal_fullstatuncertainty") );
+  
+  std::cout << cc.size() << std::endl;
+
+  char pdfname[200], tempname[200];
+  sprintf(pdfname, "data_formatted_systematics_new.pdf");
+  if(!adjust)
+      sprintf(pdfname, "data_formatted_systematics_NoChange.pdf");
+
+  for(int ii=0; ii< cc.size(); ii++) {
+    if(ii==0) {
+      sprintf(tempname, "%s(", pdfname);
+      cc[ii]->Print(tempname);
+    } else if(ii==cc.size()-1) {
+      sprintf(tempname, "%s)", pdfname);
+      cc[ii]->Print(tempname);
+    } else {
+      sprintf(tempname, "%s", pdfname);
+      cc[ii]->Print(tempname);
+    }
+  }
+
+  fout->Close();
+  
+}
+
+TCanvas* adjustUncertainties(TFile *file, TFile *fout, bool adjust, char *hname) {
+
+  std::cout << "Adjusting the bin contents of " << hname << " : "  << adjust << std::endl;
+
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
+
+  //sprintf(hname, "%sUp", hname);
+  TH1D *h_up  = (TH1D*) file->FindObjectAny(hname);
+
+  char cname[200];
+  sprintf(cname, "c_%s", hname);
+  TCanvas *c = new TCanvas(cname, cname, 900, 700);
+  h_up ->Draw();
+
+  TLegend *leg = new TLegend(0.45, 0.8, 0.8, 0.88);
+  h_up->GetYaxis()->SetRangeUser(0.87, 1.12);
+  if(hname=="searchBin_IsoTrkVetoEffUncertaintyStat" || hname=="QCDBin_HiDphi_IsoTrkVetoEffUncertaintyStat" || hname=="QCDBin_LowDphi_IsoTrkVetoEffUncertaintyStat" ) {
+    h_up->GetYaxis()->SetRangeUser(0.87, 1.2);
+    if(adjust) {
+      for(int ibin=0; ibin<h_up->GetNbinsX(); ibin++) {
+	//if(h_up->GetBinContent(ibin)>1.05 || h_up->GetBinContent(ibin)<0.95)
+	//std::cout << hname << " up: ibin " << ibin << " unc " << h_up->GetBinContent(ibin) << std::endl;
+	if(h_up->GetBinContent(ibin)>1.05)
+	  h_up->SetBinContent(ibin, 1.05);
+      }
+    }
+
+  } else if(hname=="searchBin_MuFromTauStat" || hname=="QCDBin_HiDphi_MuFromTauStat" || hname=="QCDBin_LowDphi_MuFromTauStat") {
+    h_up->GetYaxis()->SetRangeUser(0.87, 1.12);
+    if(adjust) {
+      for(int ibin=0; ibin<h_up->GetNbinsX(); ibin++) {
+	//if(h_up->GetBinContent(ibin)>1.04)
+	//std::cout << hname << " up: ibin " << ibin << " unc " << h_up->GetBinContent(ibin) << std::endl;
+	if(h_up->GetBinContent(ibin)>1.04)
+	  h_up->SetBinContent(ibin, 1.04);
+      }
+    }
+    
+  } else if(hname=="searchBin_StatUncertainties" || hname=="QCDBin_HiDphi_StatUncertainties" || hname=="QCDBin_LowDphi_StatUncertainties")
+    h_up->GetYaxis()->SetRangeUser(0.2, 4.0);
+  
+  else if(hname=="searchBin_closureUncertainty" || hname=="QCDBin_HiDphi_closureUncertainty" || hname=="QCDBin_LowDphi_closureUncertainty")
+    h_up->GetYaxis()->SetRangeUser(1.0, 2.2);
+
+  else if(hname=="searchBin_closureUncertainty_adhoc" || hname=="QCDBin_HiDphi_closureUncertainty_adhoc" || hname=="QCDBin_LowDphi_closureUncertainty_adhoc")
+    h_up->GetYaxis()->SetRangeUser(0.9, 1.4);
+
+  else if(hname=="searchBin_nominal_fullstatuncertainty" || hname=="QCDBin_HiDphi_nominal_fullstatuncertainty" || hname=="QCDBin_LowDphi_nominal_fullstatuncertainty"){
+    h_up->GetYaxis()->SetRangeUser(0.01, 10000.0);
+    c->SetLogy(1);
+  }
+    
+  leg->SetHeader(hname);
+  leg->SetLineColor(10);
+  leg->Draw("same");
+
+  fout->cd(); h_up->Write();
+  return c;
+}
+
+TCanvas* adjustUncertaintiesUpDn(TFile *file, TFile *fout, bool adjust, char *hname) {
+
+  std::cout << "Adjusting the bin contents of " << hname << " : "  << adjust << std::endl;
+  
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
+  
+  
+  char hname2[200];
+  sprintf(hname2, "%sUp", hname);
+  TH1D *h_up  = (TH1D*) file->FindObjectAny(hname2);
+  sprintf(hname2, "%sDn", hname);
+  TH1D *h_dn  = (TH1D*) file->FindObjectAny(hname2);
+  h_dn->SetLineColor  (ic_dn);
+  h_dn->SetMarkerColor(ic_dn);
+  
+  char cname[200];
+  sprintf(cname, "c_%s", hname);
+  TCanvas *c = new TCanvas(cname, cname, 900, 700);
+  h_up ->Draw();
+  h_dn ->Draw("sames");
+  
+  TLegend *leg = new TLegend(0.45, 0.8, 0.8, 0.88);
+  h_up->GetYaxis()->SetRangeUser(0.87, 1.12);
+  h_dn->GetYaxis()->SetRangeUser(0.87, 1.12);
+  if(hname == "searchBin_BMistag" || hname == "QCDBin_HiDphi_BMistag" || hname == "QCDBin_LowDphi_BMistag") {
+    leg = new TLegend(0.45, 0.8, 0.8, 0.88);
+    h_up->GetYaxis()->SetRangeUser(0.87, 1.12);
+    h_dn->GetYaxis()->SetRangeUser(0.87, 1.12);
+    if(adjust) {
+      for(int ibin=0; ibin<h_up->GetNbinsX(); ibin++) {
+	//if(h_up->GetBinContent(ibin)>1.05 || h_up->GetBinContent(ibin)<0.95)
+	  //std::cout << hname << " up: ibin " << ibin << " unc " << h_up->GetBinContent(ibin) << std::endl;
+	if(hname == "searchBin_BMistag" && (ibin==39 || ibin==80 || ibin==151) )
+	  h_up->SetBinContent(ibin, 1.05);
+	else if(hname == "QCDBin_HiDphi_BMistag" && (ibin==42 || ibin==51 || ibin==104 || ibin==199) )
+	  h_up->SetBinContent(ibin, 1.05);
+	else if(hname == "QCDBin_LowDphi_BMistag" && (ibin==51 || ibin==52 || ibin==186 || ibin==189 || ibin==199) )
+	  h_up->SetBinContent(ibin, 1.10);
+      }
+      for(int ibin=0; ibin<h_dn->GetNbinsX(); ibin++) {
+	//if(h_dn->GetBinContent(ibin)>1.05 || h_dn->GetBinContent(ibin)<0.95)
+	//std::cout << hname << " dn: ibin " << ibin << " unc " << h_dn->GetBinContent(ibin) << std::endl;
+	if(hname == "searchBin_BMistag" && (ibin==39 || ibin==80 || ibin==151) )
+	  h_dn->SetBinContent(ibin, 0.95);
+	else if(hname == "QCDBin_HiDphi_BMistag" && (ibin==42 || ibin==51 || ibin==104 || ibin==199) )
+	  h_dn->SetBinContent(ibin, 0.95);
+	else if(hname == "QCDBin_LowDphi_BMistag" && (ibin==51 || ibin==52 || ibin==186 || ibin==189 || ibin==199) )
+	  h_dn->SetBinContent(ibin, 0.9);
+      }
+    }
+      
+  } else if(hname == "searchBin_MuRecoSys" || hname == "searchBin_MuIsoSys" || hname == "searchBin_MuRecoIso") {
+    leg = new TLegend(0.45, 0.8, 0.8, 0.88);
+    h_up->GetYaxis()->SetRangeUser(0.87, 1.12);
+    h_dn->GetYaxis()->SetRangeUser(0.87, 1.12);
+    
+  } else if(hname == "searchBin_JECSys") {
+    leg = new TLegend(0.45, 0.8, 0.8, 0.88);
+    h_up->GetYaxis()->SetRangeUser(0.87, 1.12);
+    h_dn->GetYaxis()->SetRangeUser(0.87, 1.12);
+
+    if(adjust) {
+      for(int ibin=0; ibin<h_up->GetNbinsX(); ibin++) {
+	if(h_up->GetBinContent(ibin)>1.07 || h_up->GetBinContent(ibin)<0.93)
+	  std::cout << "up: ibin " << ibin << " unc " << h_up->GetBinContent(ibin) << std::endl;
+	if(ibin==131 || ibin==151 || ibin==154)
+	  h_up->SetBinContent(ibin, 0.93);
+      }
+      for(int ibin=0; ibin<h_dn->GetNbinsX(); ibin++) {
+	if(h_dn->GetBinContent(ibin)>1.07 || h_dn->GetBinContent(ibin)<0.93)
+	  std::cout << "dn: ibin " << ibin << " unc " << h_dn->GetBinContent(ibin) << std::endl;
+	if(ibin==131 || ibin==151 || ibin==154)
+	  h_dn->SetBinContent(ibin, 1.07);
+      }
+    }
+
+  } else if(hname == "searchBin_MTSys") {
+    leg = new TLegend(0.45, 0.8, 0.8, 0.88);
+    h_up->GetYaxis()->SetRangeUser(0.87, 1.12);
+    h_dn->GetYaxis()->SetRangeUser(0.87, 1.12);
+    
+  } else if(hname == "searchBin_AccSysPDF" || hname == "QCDBin_HiDphi_AccSysPDF" || hname == "QCDBin_LowDphi_AccSysPDF") {
+    leg = new TLegend(0.45, 0.8, 0.8, 0.88);
+    h_up->GetYaxis()->SetRangeUser(0.87, 1.12);
+    h_dn->GetYaxis()->SetRangeUser(0.87, 1.12);
+    if(adjust) {
+      for(int ibin=0; ibin<h_up->GetNbinsX(); ibin++) {
+	if(h_up->GetBinContent(ibin)>1.05 || h_up->GetBinContent(ibin)<0.95)
+	  std::cout << hname << " up: ibin " << ibin << " unc " << h_up->GetBinContent(ibin) << std::endl;
+      }
+      for(int ibin=0; ibin<h_dn->GetNbinsX(); ibin++) {
+	if(h_dn->GetBinContent(ibin)>1.05 || h_dn->GetBinContent(ibin)<0.95)
+	  std::cout << hname << " dn: ibin " << ibin << " unc " << h_dn->GetBinContent(ibin) << std::endl;
+	if(hname == "searchBin_AccSysPDF" && (ibin==49 || ibin==50 || ibin==59 || ibin==60 || ibin==69 || ibin==70 || ibin==79 || ibin==80) )
+	  h_dn->SetBinContent(ibin, 1.05);
+	else if(hname == "QCDBin_HiDphi_AccSysPDF" && (ibin==64 || ibin==65 || ibin==77 || ibin==78 || ibin==90 || ibin==91 || ibin==103 || ibin==104) )
+	  h_dn->SetBinContent(ibin, 1.05);
+      }
+    }
+    
+  } else if(hname == "searchBin_AccSysScale") {
+    leg = new TLegend(0.45, 0.8, 0.8, 0.88);
+    h_up->GetYaxis()->SetRangeUser(0.87, 1.12);
+    h_dn->GetYaxis()->SetRangeUser(0.87, 1.12);
+  }
+  
+  leg->SetHeader(hname);
+  leg->SetLineColor(10);
+  leg->Draw("same");
+
+  fout->cd(); h_up->Write(); h_dn->Write();
+  
+  return c;
+}
