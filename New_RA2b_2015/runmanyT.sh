36378f09525e676ec431f723bfb987c8312f996d
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2016-11-20 18:24:08 -0600

    revert to kodiak script on this branch

--- New_RA2b_2015/runmanyT.sh
+++ New_RA2b_2015/runmanyT.sh
@@ -1,172 +1,82 @@
 #!/bin/sh
 
-type=$1 # 0 prediction, 1 expectation
+  submitscript=submitScriptT.sh
+  submitscript1=submitScriptT_Expectation.sh
+
+type=$1
 outStr=$2
 
-export SUBMIT_DIR=`pwd -P`
+  mkdir -p qsub
 
-#for TStr in HT_1200_2500 HT_600_800 HT_800_1200 HT_2500_Inf DiLept Inclusive T_SingleLep Tbar_SingleLep; do
-#for TStr in t_top tW_top t_antitop tW_antitop s_channel; do
-for TStr in tW_top; do
+if [ $type -eq 0 ]; then
 
-    export SubmitFile=submitScriptT_${TStr}.jdl
-    if [ -e ${SubmitFile} ]; then
-	rm ${SubmitFile}
-    fi
-    let a=1    
-    Njobs=`ls InputFiles_T/filelist_Spring15_ST_${TStr}_* | wc -l`
-    njobs=`expr $Njobs - $a`
+  for TStr in t_top tW_top s_channel; do 
+
+    njobs=`ls InputFiles_T/filelist_Spring15_ST_${TStr}_* | wc -l`
+    #njobs=$[$njobs+1]
     echo number of jobs: $njobs
-    mkdir -p qsub
-    
-    for i in `seq 0 $njobs`; do
-	
-	export filenum=$i
-	export outStr=$outStr
-	#echo $filenum
-	#echo $code 
-	export TStr=$TStr
-	export Suffix=${TStr}_${outStr}_$filenum
-	if [ $filenum -lt 10 ]
-	then
-	    export ArgTwo=filelist_Spring15_ST_${TStr}_0$filenum
-	    export ArgTwoB=InputFiles_T/${ArgTwo}
-	else
-	    export ArgTwo=filelist_Spring15_ST_${TStr}_$filenum
-	    export ArgTwoB=InputFiles_T/${ArgTwo}
-	fi
-	export ArgThree=T_${Suffix}
-	export ArgFive=00
-	export ArgSix=0
-	export ArgSeven=$SUBMIT_DIR
-	export Output=qsub/condor_${Suffix}.out
-	export Error=qsub/condor_${Suffix}.err
-	export Log=qsub/condor_${Suffix}.log
-	export Proxy=\$ENV\(X509_USER_PROXY\)
-	
-	source /cvmfs/cms.cern.ch/cmsset_default.sh
+    #for i in `seq 1 $njobs`; do
+      for i in `seq 0 $njobs`; do
 
-	cd $SUBMIT_DIR
-	eval `scram runtime -sh`
-	echo "ROOTSYS"  ${ROOTSYS}
-	
-	if [ $type -eq 0 ]; then
-	    echo $filenum
-	    export ArgFour=TauHad2Multiple
-	    export ArgOne=./run_tauHad2    
-# echo $ArgOne
-# echo $ArgTwo
-# echo $ArgThree
-# echo $ArgFour
-# echo $ArgFive
-# echo $ArgSix
-	    echo $Output
-# echo $Error
-# echo $Log
-# echo $Proxy
-#
-	    export Output_root_file=HadTauEstimation_${TStr}_${outStr}_${i}_00.root
-	    
-	    
-	    
-	    if [ -e TauHad2Multiple/${Output_root_file} ]; then
-		echo warning !
-		echo exist TauHad2Multiple/${Output_root_file}
-	    else
-		echo submitting TauHad2Multiple/${Output_root_file}
-		
-        #
-        # Creating the submit .jdl file
-        #
-		if [ $i -eq 0 ]; then
-		    echo executable = submitScript_condor.sh>> ${SubmitFile}
-		    echo universe =vanilla>> ${SubmitFile}
-		    echo x509userproxy = ${Proxy}>> ${SubmitFile}
-		    echo notification = never>> ${SubmitFile}
-		    echo should_transfer_files = YES>> ${SubmitFile}
-		    echo WhenToTransferOutput = ON_EXIT>> ${SubmitFile}
-		 
-		fi
-		
-		echo "">> ${SubmitFile}
-		echo Arguments =${ArgOne} ${ArgTwo} ${ArgThree} ${ArgFour} ${ArgFive} ${ArgSix} ${ArgSeven}>> ${SubmitFile} 
-		echo Output = ${Output}>> ${SubmitFile}
-		echo Error = ${Error}>> ${SubmitFile}
-		echo Log = ${Log}>> ${SubmitFile}
-		echo Transfer_Input_Files = ${SUBMIT_DIR}/run_tauHad2,${SUBMIT_DIR}/TauHad,${SUBMIT_DIR}/TauHad2,${SUBMIT_DIR}/${ArgTwoB}>> ${SubmitFile}
-		echo Transfer_Output_Files = HadTauEstimation_${TStr}_${outStr}_${i}_00.root','MuJetMatchRate_${TStr}_${outStr}_${i}_00.root>> ${SubmitFile}
-		#echo Transfer_Output_Files = MuJetMatchRate_${TStr}_${outStr}_${i}_00.root>> ${SubmitFile}        
-		echo transfer_output_remaps = '"'HadTauEstimation_${TStr}_${outStr}_${i}_00.root = TauHad2Multiple/HadTauEstimation_${TStr}_${outStr}_${i}_00.root';'MuJetMatchRate_${TStr}_${outStr}_${i}_00.root = TauHad2Multiple/MuJetMatchRate_${TStr}_${outStr}_${i}_00.root'"'>> ${SubmitFile}
-#		echo transfer_output_remaps = '"'MuJetMatchRate_${TStr}_${outStr}_${i}_00.root = TauHad2Multiple/MuJetMatchRate_${TStr}_${outStr}_${i}_00.root'"'>> ${SubmitFile}
-		
-		echo queue>> ${SubmitFile}	
-	    	
-	    fi
-	    
-	fi
-	
+        export filenum=$i
+        export TStr=$TStr
+        export outStr=$outStr
+        echo $filenum
+        qsub -l walltime=12:00:00 -N $TStr_$outStr -o qsub/ -e qsub/ -V $submitscript  
 
-	if [ $type -eq 1 ]; then
-	    echo $filenum
-	    export ArgFour=TauHadMultiple
-	    export ArgOne=./run_tauHad
-#	    echo $ArgOne
-#	    echo $ArgTwo
-#	    echo $ArgThree
-#	    echo $ArgFour
-#	    echo $ArgFive
-#	    echo $ArgSix
-	    echo $Output
-#	    echo $Error
-#	    echo $Log
-#	    echo $Proxy
-#
-	    export Output_root_file=GenInfo_HadTauEstimation_${TStr}_${outStr}_${i}_00.root
-	    
-	    if [ -e TauHadMultiple/${Output_root_file} ]; then
-		echo warning !
-		echo exist TauHadMultiple/${Output_root_file}
-	    else
-		echo submitting TauHadMultiple/${Output_root_file}
-		
-        #
-        # Creating the submit .jdl file
-        #
-		if [ $i -eq 0 ]; then
-		    echo executable = submitScript_condor.sh>> ${SubmitFile}
-		    echo universe =vanilla>> ${SubmitFile}
-		    echo x509userproxy = ${Proxy}>> ${SubmitFile}
-		    echo notification = never>> ${SubmitFile}
-		    echo should_transfer_files = YES>> ${SubmitFile}
-		    echo WhenToTransferOutput = ON_EXIT>> ${SubmitFile}
-		fi
-		
-		echo "">> ${SubmitFile}
-		echo Arguments =${ArgOne} ${ArgTwo} ${ArgThree} ${ArgFour} ${ArgFive} ${ArgSix} ${ArgSeven}>> ${SubmitFile} 
-		echo Output = ${Output}>> ${SubmitFile}
-		echo Error = ${Error}>> ${SubmitFile}
-		echo Log = ${Log}>> ${SubmitFile}
-		echo Transfer_Input_Files = ${SUBMIT_DIR}/run_tauHad,${SUBMIT_DIR}/TauHad,${SUBMIT_DIR}/TauHad2,${SUBMIT_DIR}/${ArgTwoB}>> ${SubmitFile}
+        sleep 1
 
-		echo Transfer_Output_Files = GenInfo_HadTauEstimation_${TStr}_${outStr}_${i}_00.root','FailRate_GenTau_jet_${TStr}_${outStr}_${i}_00.root>> ${SubmitFile}        
+      done
 
-#		echo Transfer_Output_Files = GenInfo_HadTauEstimation_${TStr}_${outStr}_${i}_00.root','FailRate_GenTau_jet_${TStr}_${outStr}_${i}_00.root','HadTau_TauResponseTemplates_${TStr}_${outStr}_${i}_00.root','IsoEfficiencies_${TStr}_${outStr}_${i}_00.root','LostLepton2_MuonEfficienciesFrom${TStr}_${outStr}_${i}_00.root','Probability_Tau_mu_${TStr}_${outStr}_${i}_00.root','TauBtaggedRate_${TStr}_${outStr}_${i}_00.root','TriggerEff_${TStr}_${outStr}_${i}_00.root>> ${SubmitFile}        
-		echo transfer_output_remaps = '"'GenInfo_HadTauEstimation_${TStr}_${outStr}_${i}_00.root = TauHadMultiple/GenInfo_HadTauEstimation_${TStr}_${outStr}_${i}_00.root';'FailRate_GenTau_jet_${TStr}_${outStr}_${i}_00.root = TauHadMultiple/FailRate_GenTau_jet_${TStr}_${outStr}_${i}_00.root'"'>> ${SubmitFile}
+  done
 
-#		echo transfer_output_remaps = '"'GenInfo_HadTauEstimation_${TStr}_${outStr}_${i}_00.root = TauHadMultiple/GenInfo_HadTauEstimation_${TStr}_${outStr}_${i}_00.root';'FailRate_GenTau_jet_${TStr}_${outStr}_${i}_00.root = TauHadMultiple/FailRate_GenTau_jet_${TStr}_${outStr}_${i}_00.root';'HadTau_TauResponseTemplates_${TStr}_${outStr}_${i}_00.root = TauHadMultiple/HadTau_TauResponseTemplates_${TStr}_${outStr}_${i}_00.root';'IsoEfficiencies_${TStr}_${outStr}_${i}_00.root = TauHadMultiple/IsoEfficiencies_${TStr}_${outStr}_${i}_00.root';'LostLepton2_MuonEfficienciesFrom${TStr}_${outStr}_${i}_00.root = TauHadMultiple/LostLepton2_MuonEfficienciesFrom${TStr}_${outStr}_${i}_00.root';'Probability_Tau_mu_${TStr}_${outStr}_${i}_00.root = TauHadMultiple/Probability_Tau_mu_${TStr}_${outStr}_${i}_00.root';'TauBtaggedRate_${TStr}_${outStr}_${i}_00.root = TauHadMultiple/TauBtaggedRate_${TStr}_${outStr}_${i}_00.root';'TriggerEff_${TStr}_${outStr}_${i}_00.root = TauHadMultiple/TriggerEff_${TStr}_${outStr}_${i}_00.root'"'>> ${SubmitFile}
-		echo queue>> ${SubmitFile}	
-	    fi
-	    
-	fi
-	
-#	sleep 1
 
-    done
-    
-  #
-  # Actual submission
-  # 
-    condor_submit ${SubmitFile}
+  submitscript=submitScriptT.sh
 
-done
+  for TStr in t_antitop tW_antitop ; do                          
+
+    njobs=`ls InputFiles_T/filelist_Spring15_ST_${TStr}_* | wc -l`
+    #njobs=$[$njobs+1]
+    echo number of jobs: $njobs
+    #for i in `seq 1 $njobs`; do
+      for i in `seq 0 $njobs`; do
+
+        export filenum=$i
+        export TStr=$TStr
+        export outStr=$outStr
+        echo $filenum
+        qsub -l walltime=12:00:00 -N $TStr_$outStr -o qsub/ -e qsub/ -V $submitscript 
+
+        sleep 1
+
+      done
+
+  done
+
+fi
+
+if [ $type -eq 1 ]; then
+
+  for TStr in t_top tW_top t_antitop tW_antitop s_channel; do
+
+    njobs=`ls InputFiles_T/filelist_Spring15_ST_${TStr}_* | wc -l`
+    #njobs=$[$njobs+1]
+    echo number of jobs: $njobs
+    #for i in `seq 1 $njobs`; do
+      for i in `seq 0 $njobs`; do
+
+        export filenum=$i
+        export TStr=$TStr
+        export outStr=$outStr
+        echo $filenum
+        qsub -l walltime=12:00:00 -N $TStr_$outStr -o qsub/ -e qsub/ -V $submitscript1 
+
+        sleep 1
+
+      done
+
+  done
+
+
+fi
 
--- New_RA2b_2015/runmanyWJet.sh
+++ New_RA2b_2015/runmanyWJet.sh
@@ -1,174 +1,74 @@
 #!/bin/sh
 
-type=$1 # 0 prediction, 1 expectation
+type=$1
 outStr=$2
 
-export SUBMIT_DIR=`pwd -P`
+submitscript=submitScriptWJet.sh
+submitscript1=submitScriptWJet_Expectation.sh
 
-#for WJetStr in 100_200 200_400 400_600 600_800 800_1200 1200_2500 2500_Inf; do
-for WJetStr in 100_200; do
-    export SubmitFile=submitScriptWJet_${WJetStr}.jdl
-    if [ -e ${SubmitFile} ]; then
-	rm ${SubmitFile}
-    fi
-    let a=1
-    Njobs=`ls InputFiles_WJet_${WJetStr}/filelist_Spring15_WJet_HT_${WJetStr}_* | wc -l`
-    njobs=`expr $Njobs - $a`
-    echo number of jobs: $njobs
-    mkdir -p qsub
-    
-    for i in `seq 0 $njobs`; do
-	
-	export filenum=$i
-	export outStr=$outStr
-	echo $filenum
-	echo $code 
-	export WJetStr=$WJetStr
-	export Suffix=${WJetStr}_${outStr}_$filenum
-	if [ $filenum -lt 10 ]
-	then
-	    export ArgTwo=filelist_Spring15_WJet_HT_${WJetStr}_00$filenum
-	    export ArgTwoB=InputFiles_WJet_${WJetStr}/${ArgTwo}
-	elif [ $filenum -lt 100 ]
-	then
-	    export ArgTwo=filelist_Spring15_WJet_HT_${WJetStr}_0$filenum
-	    export ArgTwoB=InputFiles_WJet_${WJetStr}/${ArgTwo}
-	else
-	    export ArgTwo=filelist_Spring15_WJet_HT_${WJetStr}_$filenum
-	    export ArgTwoB=InputFiles_WJet_${WJetStr}/${ArgTwo}
-	fi
-	export ArgThree=WJet_${Suffix}
-	export ArgFive=00
-	export ArgSix=0
-	export ArgSeven=$SUBMIT_DIR
-	export Output=qsub/condor_${Suffix}.out
-	export Error=qsub/condor_${Suffix}.err
-	export Log=qsub/condor_${Suffix}.log
-	export Proxy=\$ENV\(X509_USER_PROXY\)
-	
-	source /cvmfs/cms.cern.ch/cmsset_default.sh
+  mkdir -p qsub
 
-	cd $SUBMIT_DIR
-	eval `scram runtime -sh`
-	echo "ROOTSYS"  ${ROOTSYS}
-	
-	if [ $type -eq 0 ]; then
-	    echo $filenum
-	    export ArgFour=TauHad2Multiple
-	    export ArgOne=./run_tauHad2    
-#   echo $ArgOne
-#   echo $ArgTwo
-#   echo $ArgThree
-#   echo $ArgFour
-#   echo $ArgFive
-#   echo $ArgSix
-	    echo $Output
-#   echo $Error
-#   echo $Log
-#   echo $Proxy
-#
-	    export Output_root_file=HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root
-	    
-	    
-	    
-	    if [ -e TauHad2Multiple/${Output_root_file} ]; then
-		echo warning !
-		echo exist TauHad2Multiple/${Output_root_file}
-	    else
-		echo submitting TauHad2Multiple/${Output_root_file}
-		
-        #
-        # Creating the submit .jdl file
-        #
-		if [ $i -eq 0 ]; then
-		    echo executable = submitScript_condor.sh>> ${SubmitFile}
-		    echo universe =vanilla>> ${SubmitFile}
-		    echo x509userproxy = ${Proxy}>> ${SubmitFile}
-		    echo notification = never>> ${SubmitFile}
-		    echo should_transfer_files = YES>> ${SubmitFile}
-		    echo WhenToTransferOutput = ON_EXIT>> ${SubmitFile}
-		
-    
-		fi
-		
-		echo "">> ${SubmitFile}
-		echo Arguments =${ArgOne} ${ArgTwo} ${ArgThree} ${ArgFour} ${ArgFive} ${ArgSix} ${ArgSeven}>> ${SubmitFile} 
-		echo Output = ${Output}>> ${SubmitFile}
-		echo Error = ${Error}>> ${SubmitFile}
-		echo Log = ${Log}>> ${SubmitFile}
-		echo Transfer_Input_Files = ${SUBMIT_DIR}/run_tauHad2,${SUBMIT_DIR}/TauHad,${SUBMIT_DIR}/TauHad2,${SUBMIT_DIR}/${ArgTwoB}>> ${SubmitFile}
-		echo Transfer_Output_Files = HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root','MuJetMatchRate_WJet_${WJetStr}_${outStr}_${i}_00.root>> ${SubmitFile}
-		#echo Transfer_Output_Files = MuJetMatchRate_WJet_${WJetStr}_${outStr}_${i}_00.root>> ${SubmitFile}        
-		echo transfer_output_remaps = '"'HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHad2Multiple/HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root';'MuJetMatchRate_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHad2Multiple/MuJetMatchRate_WJet_${WJetStr}_${outStr}_${i}_00.root'"'>> ${SubmitFile}
-#		echo transfer_output_remaps = '"'MuJetMatchRate_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHad2Multiple/MuJetMatchRate_WJet_${WJetStr}_${outStr}_${i}_00.root'"'>> ${SubmitFile}
-		
-		echo queue>> ${SubmitFile}	
-	    	
-	    fi
-	    
-	fi
-	
+if [ $type -eq 0 ]; then
+  for WJetStr in 100_200 200_400 400_600 600_800 800_1200 1200_2500 2500_Inf; do 
 
-	if [ $type -eq 1 ]; then
-	    echo $filenum
-	    export ArgFour=TauHadMultiple
-	    export ArgOne=./run_tauHad
-#	    echo $ArgOne
-#	    echo $ArgTwo
-#	    echo $ArgThree
-#	    echo $ArgFour
-#	    echo $ArgFive
-#	    echo $ArgSix
-	    echo $Output
-#	    echo $Error
-#	    echo $Log
-#	    echo $Proxy
-#
-	    export Output_root_file=GenInfo_HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root
-	    
-	    if [ -e TauHadMultiple/${Output_root_file} ]; then
-		echo warning !
-		echo exist TauHadMultiple/${Output_root_file}
-	    else
-		echo submitting TauHadMultiple/${Output_root_file}
-		
-        #
-        # Creating the submit .jdl file
-        #
-		if [ $i -eq 0 ]; then
-		    echo executable = submitScript_condor.sh>> ${SubmitFile}
-		    echo universe =vanilla>> ${SubmitFile}
-		    echo x509userproxy = ${Proxy}>> ${SubmitFile}
-		    echo notification = never>> ${SubmitFile}
-		    echo should_transfer_files = YES>> ${SubmitFile}
-		    echo WhenToTransferOutput = ON_EXIT>> ${SubmitFile}
-		fi
-		
-		echo "">> ${SubmitFile}
-		echo Arguments =${ArgOne} ${ArgTwo} ${ArgThree} ${ArgFour} ${ArgFive} ${ArgSix} ${ArgSeven}>> ${SubmitFile} 
-		echo Output = ${Output}>> ${SubmitFile}
-		echo Error = ${Error}>> ${SubmitFile}
-		echo Log = ${Log}>> ${SubmitFile}
-		echo Transfer_Input_Files = ${SUBMIT_DIR}/run_tauHad,${SUBMIT_DIR}/TauHad,${SUBMIT_DIR}/TauHad2,${SUBMIT_DIR}/${ArgTwoB}>> ${SubmitFile}
-		echo Transfer_Output_Files = GenInfo_HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root','FailRate_GenTau_jet_WJet_${WJetStr}_${outStr}_${i}_00.root','HadTau_TauResponseTemplates_WJet_${WJetStr}_${outStr}_${i}_00.root','IsoEfficiencies_WJet_${WJetStr}_${outStr}_${i}_00.root','LostLepton2_MuonEfficienciesFromWJet_${WJetStr}_${outStr}_${i}_00.root','Probability_Tau_mu_WJet_${WJetStr}_${outStr}_${i}_00.root','TauBtaggedRate_WJet_${WJetStr}_${outStr}_${i}_00.root>> ${SubmitFile}        
 
-#		echo Transfer_Output_Files = GenInfo_HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root','FailRate_GenTau_jet_WJet_${WJetStr}_${outStr}_${i}_00.root','HadTau_TauResponseTemplates_WJet_${WJetStr}_${outStr}_${i}_00.root','IsoEfficiencies_WJet_${WJetStr}_${outStr}_${i}_00.root','LostLepton2_MuonEfficienciesFromWJet_${WJetStr}_${outStr}_${i}_00.root','Probability_Tau_mu_WJet_${WJetStr}_${outStr}_${i}_00.root','TauBtaggedRate_WJet_${WJetStr}_${outStr}_${i}_00.root','TriggerEff_WJet_${WJetStr}_${outStr}_${i}_00.root>> ${SubmitFile}        
-		echo transfer_output_remaps = '"'GenInfo_HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/GenInfo_HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root';'FailRate_GenTau_jet_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/FailRate_GenTau_jet_WJet_${WJetStr}_${outStr}_${i}_00.root';'HadTau_TauResponseTemplates_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/HadTau_TauResponseTemplates_WJet_${WJetStr}_${outStr}_${i}_00.root';'IsoEfficiencies_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/IsoEfficiencies_WJet_${WJetStr}_${outStr}_${i}_00.root';'LostLepton2_MuonEfficienciesFromWJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/LostLepton2_MuonEfficienciesFromWJet_${WJetStr}_${outStr}_${i}_00.root';'Probability_Tau_mu_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/Probability_Tau_mu_WJet_${WJetStr}_${outStr}_${i}_00.root';'TauBtaggedRate_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/TauBtaggedRate_WJet_${WJetStr}_${outStr}_${i}_00.root'"'>> ${SubmitFile}
+      njobs=`ls InputFiles_WJet_${WJetStr}/filelist_Spring15_WJet_HT_${WJetStr}* | wc -l`
+      #njobs=$[$njobs+1]
+      echo number of jobs: $njobs
+      #for i in `seq 1 $njobs`; do
+        for i in `seq 0 $njobs`; do
 
-#		echo transfer_output_remaps = '"'GenInfo_HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/GenInfo_HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root';'FailRate_GenTau_jet_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/FailRate_GenTau_jet_WJet_${WJetStr}_${outStr}_${i}_00.root';'HadTau_TauResponseTemplates_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/HadTau_TauResponseTemplates_WJet_${WJetStr}_${outStr}_${i}_00.root';'IsoEfficiencies_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/IsoEfficiencies_WJet_${WJetStr}_${outStr}_${i}_00.root';'LostLepton2_MuonEfficienciesFromWJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/LostLepton2_MuonEfficienciesFromWJet_${WJetStr}_${outStr}_${i}_00.root';'Probability_Tau_mu_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/Probability_Tau_mu_WJet_${WJetStr}_${outStr}_${i}_00.root';'TauBtaggedRate_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/TauBtaggedRate_WJet_${WJetStr}_${outStr}_${i}_00.root';'TriggerEff_WJet_${WJetStr}_${outStr}_${i}_00.root = TauHadMultiple/TriggerEff_WJet_${WJetStr}_${outStr}_${i}_00.root'"'>> ${SubmitFile}
-		echo queue>> ${SubmitFile}	
-	    fi
-	    
-	fi
-	
-	#sleep 1
+          export filenum=$i
+          export WJetStr=$WJetStr
+          export outStr=$outStr
+          echo $filenum
+          if [ -e TauHad2Multiple/HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root ]; then
+            echo warning !
+            echo exist TauHad2Multiple/HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root
+          else
+            echo submitting TauHad2Multiple/HadTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root
+            qsub -l walltime=12:00:00 -N WJet_$WJetStr_$outStr -o qsub/ -e qsub/ -V $submitscript  
+          fi
 
-    done
-    
-  #
-  # Actual submission
-  # 
-    condor_submit ${SubmitFile}
+          sleep 1
 
-done
+        done
+
+
+  done
+
+fi
+
+
+if [ $type -eq 1 ]; then
+
+  for WJetStr in 100_200 200_400 400_600 600_800 800_1200 1200_2500 2500_Inf; do
+
+      njobs=`ls InputFiles_WJet_${WJetStr}/filelist_Spring15_WJet_HT_${WJetStr}* | wc -l`
+      #njobs=$[$njobs+1]
+      echo number of jobs: $njobs
+      #for i in `seq 1 $njobs`; do
+        for i in `seq 0 $njobs`; do
+
+          export filenum=$i
+          export WJetStr=$WJetStr
+          export outStr=$outStr
+          echo $filenum
+          if [ -e TauHadMultiple/GenInfo_HaTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root ]; then
+            echo warning !
+            echo exist TauHadMultiple/GenInfo_HaTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root
+          else
+            echo submitting TauHadMultiple/GenInfo_HaTauEstimation_WJet_${WJetStr}_${outStr}_${i}_00.root          
+            qsub -l walltime=12:00:00 -N WJet_$WJetStr_$outStr -o qsub/ -e qsub/ -V $submitscript1 
+          fi
+
+          sleep 1
+
+        done
+
+  done
+
+fi
+
+
 
